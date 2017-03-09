a687979f3be9585e8277641d45020c4520986850
Maximilian Wendel <maximilian.Wendel@outlook.com>
2017-03-09 09:22:58 +0100

    Began work on quiz

--- js/modernism.js
+++ js/modernism.js
@@ -1,3 +1,18 @@
 /**
  * Copyright (c) 2017
  */
+
+var quiz = {};
+
+quiz.questions = [];
+
+quiz.questions += {
+  "question":"During what span of time did Modernism take place?",
+  "answers":[{
+    1:"1920",
+  }],
+  };
+quiz.questions[0]
+
+console.log(quiz);
+console.log(quiz.questions[0]);
