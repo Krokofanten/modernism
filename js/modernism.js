228834ff6bd8d46727f649c1eff038d1dd1e60f8
Maximilian Wendel <maximilian.Wendel@outlook.com>
2017-03-14 17:03:52 +0100

    Minor changes

--- js/modernism.js
+++ js/modernism.js
@@ -17,6 +17,46 @@ $('#secondary').hide();
    }
  });
 
+ $(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('nav-com-active');
+        })
+        $(this).addClass('nav-com-active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top()+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#secondary-sections a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#secondary-sections a').removeClass("nav-com-active");
+            currLink.addClass("nav-com-active");
+        }
+        else{
+            currLink.removeClass("nav-com-active");
+        }
+    });
+}
+
 var quiz = {};
 
 quiz.questions = {
